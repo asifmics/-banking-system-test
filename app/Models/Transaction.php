dd51856db2d5668f464fe63367a256641ef0ec5f
asifmics <asif.mics@gmail.com>
2023-10-04 03:04:16 +0600

    User can open his new account

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use App\enums\TransactionTypeEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id','transaction_type','amount','fee','date'
+    ];
+
+    protected $casts = [
+        'transaction_type' => TransactionTypeEnum::class,
+        'amount' => 'double',
+        'fee' => 'decimal',
+        'date' => 'date',
+    ];
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\enums\AccountTypeEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -19,6 +21,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'account_type',
+        'balance',
         'email',
         'password',
     ];
@@ -41,5 +45,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'account_type' => AccountTypeEnum::class,
+        'balance' => 'double'
     ];
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
